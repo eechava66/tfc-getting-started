2fddad1eede7777d60aa79a2a488513c201bb441
eechava6 <esteban.echavarria@wizeline.com>
2021-08-20 17:44:29 -0500

    re-configuring project

--- base/backend.tf
+++ base/backend.tf
@@ -1,12 +1,8 @@
 # The block below configures Terraform to use the 'remote' backend with Terraform Cloud.
 # For more information, see https://www.terraform.io/docs/backends/types/remote.html
 terraform {
-  backend "remote" {
-    organization = "{{ORGANIZATION_NAME}}"
-
-    workspaces {
-      name = "{{WORKSPACE_NAME}}"
-    }
+  backend "local" {
+    path = "tf.tfstate"
   }
 
   required_version = ">= 0.13.0"
